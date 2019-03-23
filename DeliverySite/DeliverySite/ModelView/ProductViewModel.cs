6c70018b2e38b5701b77da00696518f6122d92c4
kfir2037 <kfir2037@gmail.com>
2019-03-23 20:24:53 +0200

    ProductViewModel

---
+++ DeliverySite/DeliverySite/ModelView/ProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using DeliverySite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DeliverySite.ModelView
+{
+    public class UserViewModel
+    {
+        public Product product { get; set; }
+        public List<Product> products { get; set; }
+    }
+}
