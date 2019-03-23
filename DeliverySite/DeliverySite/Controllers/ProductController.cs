c64fb4a914dd354d33330e7777b4d0329582b9ff
Shimon Emuna <shimonemu@gmail.com>
2019-03-24 00:27:08 +0200

    product - fixes

--- DeliverySite/DeliverySite/Controllers/ProductController.cs
+++ DeliverySite/DeliverySite/Controllers/ProductController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DeliverySite.Dal;
+using DeliverySite.Models;
+using DeliverySite.ModelView;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
--- DeliverySite/DeliverySite/ModelView/ProductViewModel.cs
+++ DeliverySite/DeliverySite/ModelView/ProductViewModel.cs
@@ -6,7 +6,7 @@ using System.Web;
 
 namespace DeliverySite.ModelView
 {
-    public class UserViewModel
+    public class ProductViewModel
     {
         public Product product { get; set; }
         public List<Product> products { get; set; }
