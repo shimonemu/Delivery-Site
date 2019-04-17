b838742ef74780fb3ecb2d6c45dd15eac4a8456d
kfir2037 <kfir2037@gmail.com>
2019-04-17 10:05:46 +0300

    sprunt 2

---
+++ DeliverySite/UnitTestDeliverySite/HomeControllerTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web;
+using System.Collections.Generic;
+using System.Linq;
+using DeliverySite.Controllers;
+using System.Web.Mvc;
+using Microsoft.CSharp.RuntimeBinder;
+
+namespace UnitTestDeliverySite
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void About()
+        {
+            //Arrange
+            HomeController controller = new HomeController();
+
+            //Act
+            ViewResult result = controller.About() as ViewResult;
+
+            //Assert
+            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+        }
+
+        [TestMethod]
+        public void ChangePasswordUser()
+        {
+            //Arrange
+            UserController controller = new UserController();
+            
+            //Act
+            //ViewResult result = controller.ChangePassword() as ViewResult;
+            ViewResult result2 = controller.ConfirmChangePassword() as ViewResult;
+            
+            //Assert
+            Assert.AreEqual("The Password have to be 8 to 16 chars long", result2.TempData["NotGoodPass"]);
+
+        }
+    }
+}
